9b3a2cca5593467a1f54eb6cd782b0c1bc3122e9
RudenkoOleksii <oleksii98@gmail.com>
2019-04-22 13:50:16 +0300

    initial commit (app resetup)

---
+++ WorkAround/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorkAround.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
