e51e775f39797e8ffb07a458446de895f774417f
LyubomurBuryi <gruf.bird@gmail.com>
2019-04-23 18:59:35 +0300

    started implementing create post

---
+++ WorkAround/Controllers/PostController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorkAround.Data;
+using WorkAround.Data.Entity;
+
+namespace WorkAround.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public PostController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var posts = this.context.Post.ToList();
+            return View(posts);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ WorkAround/Models/PostsListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkAround.Data.Entity;
+
+namespace WorkAround.ViewModels
+{
+    public class PostsListViewModel
+    {
+        public List<Post> posts { get; set; }
+    }
+}
