a628fed3dfef258e5098cd438c46a30ba1d1c799
RudenkoOleksii <oleksii98@gmail.com>
2019-05-06 13:53:00 +0300

    add employerService

---
+++ WorkAround.Data/Interfaces/IEmployerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using WorkAround.Data.Entities;
+
+namespace WorkAround.Data.Interfaces
+{
+    public interface IEmployerRepository
+    {
+        IEnumerable<Employer> All();
+
+        Employer GetEmployerById(string id);
+
+        void Create(Employer employer);
+
+        void Update(Employer employer);
+
+        void Delete(string id);
+
+    }
+}
