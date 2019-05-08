f6d7d77a7f95eda29114acabdc59eef1c0bda62b
Vitalik Hashchuk <vitalikhashchuk@gmail.com>
2019-05-09 00:48:57 +0300

    add mock for ProfessionRepository

---
+++ XUnitTests/MockedRepositories/MockedProffesionRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using WorkAround.Data.Entities;
+using WorkAround.Data.Interfaces;
+
+namespace XUnitTests.MockedRepositories {
+    class MockedProffesionRepository : IProffesionRepository {
+        public List<Proffesion> Collection;
+
+        public MockedProffesionRepository() {
+            var profession = new Proffesion {Id = "mockedId", Title = "mockedName"};
+
+            Collection = new List<Proffesion> {profession, profession, profession};
+        }
+
+        public IEnumerable<Proffesion> All() {
+            return Collection;
+        }
+
+        public Proffesion GetById(string id) {
+            return Collection.Find(e => e.Id == id);
+        }
+
+        public void Create(Proffesion proffesion) {
+            Collection.Add(proffesion);
+        }
+
+        public void Update(Proffesion proffesion) {
+            var index = Collection.FindIndex(e => e.Id == proffesion.Id);
+            Collection[index] = proffesion;
+        }
+
+        public void Delete(string id) {
+            var index = Collection.FindIndex(e => e.Id == id);
+            Collection.RemoveAt(index);
+        }
+    }
+}
