8d2c9d4b21417eab82cc01a76ec604e75e7a096d
Vitalik Hashchuk <vitalikhashchuk@gmail.com>
2019-05-09 00:04:46 +0300

    complete AuthUserService tests

--- XUnitTests/AuthUserServiceTest.cs
+++ XUnitTests/AuthUserServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using WorkAround.Data.Entities;
 using WorkAround.Data.Interfaces;
@@ -19,6 +20,39 @@ namespace XUnitTests
             foreach (var user in repoData) {
                 Assert.Equal(user, user);
             }
+        }
+
+        [Fact]
+        public void TestGetCreate() {
+            var authUser = new AuthUser();
+            authUser.Id = "testId";
+
+            repo.Create(authUser);
+            
+            Assert.Equal(authUser, repo.Get("testId"));
+        }
+
+        [Fact]
+        public void TestUpdate() {
+            var authUser = repo.All().First();
+
+            authUser.Nickname = "updatedNickname";
+
+            repo.Update(authUser);
+
+            Assert.Equal(authUser, repo.Get(authUser.Id));
+        }
+
+        [Fact]
+        public void TestDelete() {
+            var authUser = new AuthUser();
+            authUser.Id = "testId";
+
+            repo.Create(authUser);
+
+            repo.Delete(authUser.Id);
+
+            Assert.Null(repo.Get(authUser.Id));
         }
     }
 }
--- XUnitTests/MockedRepositories/MockedAuthUserRepository.cs
+++ XUnitTests/MockedRepositories/MockedAuthUserRepository.cs
@@ -5,18 +5,20 @@ using WorkAround.Data.Entities;
 using WorkAround.Data.Interfaces;
 using WorkAround.Services;
 
-namespace XUnitTests.MockedRepositories
-{
-    class MockedAuthUserRepository: IAuthUserRepository
-    {
-        public IEnumerable<AuthUser> All() {
+namespace XUnitTests.MockedRepositories {
+    class MockedAuthUserRepository : IAuthUserRepository {
+        public List<AuthUser> AuthUsers;
+
+        public MockedAuthUserRepository() {
             var authUser = new AuthUser();
+            authUser.Id = "mockedId";
             authUser.Nickname = "MockedNickname";
 
             var chat = new Chat();
             chat.Id = "mockedId";
 
-            var message = new Message {
+            var message = new Message
+            {
                 Id = "mockedId",
                 Sent = new DateTime(),
                 Text = "mockedText",
@@ -24,40 +26,44 @@ namespace XUnitTests.MockedRepositories
                 User = authUser
             };
 
-            chat.Messages = new List<Message> {message, message, message};
+            chat.Messages = new List<Message> { message, message, message };
 
-            authUser.Chats = new List<Chat> {chat, chat, chat};
+            authUser.Chats = new List<Chat> { chat, chat, chat };
             authUser.Description = "mockedDescription";
 
             var profession = new Proffesion();
             profession.Id = "mockedId";
             profession.Title = "mockedName";
 
-            authUser.Proffesions = new List<Proffesion> {profession, profession, profession};
+            authUser.Proffesions = new List<Proffesion> { profession, profession, profession };
 
-            var rate = new Rate {User = authUser, Description = "mockedDescription", Id = "mockedId", Stars = 5};
+            var rate = new Rate { User = authUser, Description = "mockedDescription", Id = "mockedId", Stars = 5 };
 
-            authUser.Ratings = new List<Rate> {rate, rate, rate};
+            authUser.Ratings = new List<Rate> { rate, rate, rate };
 
-            var authUsers = new List<AuthUser> {authUser, authUser, authUser};
+            AuthUsers = new List<AuthUser> { authUser, authUser, authUser };
+        }
 
-            return authUsers;
+        public IEnumerable<AuthUser> All() {
+            return AuthUsers;
         }
 
         public AuthUser Get(string id) {
-            throw new NotImplementedException();
+            return AuthUsers.Find(user => user.Id == id);
         }
 
         public void Create(AuthUser authUser) {
-            throw new NotImplementedException();
+            AuthUsers.Add(authUser);
         }
 
         public void Update(AuthUser authUser) {
-            throw new NotImplementedException();
+            var index = AuthUsers.FindIndex(user => user.Id == authUser.Id);
+            AuthUsers[index] = authUser;
         }
 
         public void Delete(string id) {
-            throw new NotImplementedException();
+            var index = AuthUsers.FindIndex(user => user.Id == id);
+            AuthUsers.RemoveAt(index);
         }
     }
 }
