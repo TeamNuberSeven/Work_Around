f7d6c83b57378698182f5aeb17d8f25f32c6e805
Vitalik Hashchuk <vitalikhashchuk@gmail.com>
2019-05-09 01:01:51 +0300

    add ChatTest

--- XUnitTests/RepositoryTests/ChatTest.cs
+++ XUnitTests/RepositoryTests/ChatTest.cs
@@ -1,12 +1,13 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using WorkAround.Data.Entities;
 using WorkAround.Data.Interfaces;
 using Xunit;
 using XUnitTests.MockedRepositories;
 
 namespace XUnitTests.RepositoryTests {
-    public class AuthUserRepositoryTest {
-        readonly IAuthUserRepository _repository = new MockedAuthUserRepository();
+    public class ChatTest {
+        readonly IChatRepository _repository = new MockedChatRepository();
 
         [Fact]
         public void TestAll() {
@@ -19,35 +20,35 @@ namespace XUnitTests.RepositoryTests {
 
         [Fact]
         public void TestGetCreate() {
-            var entity = new AuthUser();
+            var entity = new Chat();
             entity.Id = "testId";
 
             _repository.Create(entity);
 
-            Assert.Equal(entity, _repository.Get("testId"));
+            Assert.Equal(entity, _repository.GetById("testId"));
         }
 
         [Fact]
         public void TestUpdate() {
             var entity = _repository.All().First();
 
-            entity.Nickname = "updatedNickname";
+            entity.Messages = new List<Message>();
 
             _repository.Update(entity);
 
-            Assert.Equal(entity, _repository.Get(entity.Id));
+            Assert.Equal(entity, _repository.GetById(entity.Id));
         }
 
         [Fact]
         public void TestDelete() {
-            var entity = new AuthUser();
+            var entity = new Chat();
             entity.Id = "testId";
 
             _repository.Create(entity);
 
             _repository.Delete(entity.Id);
 
-            Assert.Null(_repository.Get(entity.Id));
+            Assert.Null(_repository.GetById(entity.Id));
         }
     }
 }
